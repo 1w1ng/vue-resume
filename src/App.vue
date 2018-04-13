ad46170c60ef3657309bb88d34193ffc96270794
1w1ng <921754984@qq.com>
2018-04-13 23:03:21 +0800

    实现表单输入

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,7 @@ import './assets/reset.css'
 import Topbar from './components/Topbar'
 import ResumeEditor from './components/ResumeEditor'
 import ResumePreview from './components/ResumePreview'
+import icons from './assets/icons'
 
 export default {
   name: 'app',
@@ -29,7 +30,10 @@ export default {
       text: '你好'
     }
   },
-  components: {Topbar, ResumeEditor, ResumePreview}
+  components: {Topbar, ResumeEditor, ResumePreview},
+  created(){
+    document.body.insertAdjacentHTML('afterbegin',icons)
+  }
 }
 </script>
 
@@ -63,5 +67,12 @@ export default {
     flex-grow: 1;
     margin-left: 16px;
     background: #777;
+  }
+  svg.icon{
+    height: 1em;
+    width:1em;
+    fill:currentColor;
+    vertical-align: -0.1em;
+    font-size:16px;
   }
 </style>
--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -2,6 +2,10 @@
   <div id="topbar">
     <div class="wrapper">
       <span class="logo">Resumer</span>
+      <div class="actions">
+        <el-button type="primary">保存</el-button>
+        <el-button type="primary">预览</el-button>
+      </div>
     </div>
   </div>
 </template>
@@ -12,26 +16,26 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="scss">
   #topbar{
     background:#ffffff;
     box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
-  }
-  #topbar>.wrapper{
-    min-width: 1024px;
-    max-width: 1440px;
-    margin: 0 auto;
-    height:64px;
-  }
-  #topbar>.wrapper{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 16px;
-  }
-  #topbar .logo{
-    font-size:24px;
-    color:#000000;
+    >.wrapper{
+      min-width: 1024px;
+      max-width: 1440px;
+      margin: 0 auto;
+      height:64px;
+    }
+    >.wrapper{
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0 16px;
+    }
+    .logo{
+      font-size:24px;
+      color:#000000;
+    }
   }
 </style>
 
